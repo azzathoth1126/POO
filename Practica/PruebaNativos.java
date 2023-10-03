32d51e770d8a6178598e1c4bb1f7688f9bef30d0
Azzathoth1126 <alexis.mta2020@gmail.com>
2023-10-03 23:40:55 +0000

    Listo

--- Practica/PruebaNativos.java
+++ Practica/PruebaNativos.java
@@ -9,10 +9,15 @@ public class PruebaNativos{
     private static String cadena;
 
     public static void main (String [] args ) {
-    
+        
+        /** int hola = 5; 
+            solo las variables globales o de inicializacion, si no se inicializan 
+                con algun valor el compilador les da un valor por defecto */
+        
         System.out.println(booleano);
         System.out.println(elShort);
         System.out.println(elEntero);
+        //System.out.println(hola);
         System.out.println(elFlotante);
         System.out.println(elLong);
         System.out.println(elDoble);
