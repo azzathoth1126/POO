0043c36e51c3947d87769ae739f74d8e669f3e8f
Azzathoth1126 <alexis.mta2020@gmail.com>
2023-10-04 00:13:17 +0000

    listo

--- practica_04/ListaCaracteres.java
+++ practica_04/ListaCaracteres.java
@@ -9,11 +9,5 @@ public class ListaCaracteres {
 
 
 /** 
-public class ListaCaracteres {
-	public static void main(String[ ] args) {
-		for(char c=0;c<128;c++)
-		if(c!=26) //ANSI clear screen
-		System.out.println("valor: "+(int)c+"caracter:"+c);
-		}
-	}//fin clase
+
 */
