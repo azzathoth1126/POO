b6861679ff8d3667e7e473eb7ed20da5af6478b7
Azzathoth1126 <alexis.mta2020@gmail.com>
2023-10-11 00:07:58 +0000

    listo

---
+++ practica_04/Equivalente.java
@@ -0,0 +1,25 @@
+//Equivalente.java
+public class Equivalente {
+	public static void main( String [ ] args ) {
+		Integer n1 = new Integer( 47 );
+		Integer n2 = new Integer( 47 );
+		System.out.println( n1==n2 );
+		System.out.println( n1!=n2 );
+		}
+	}//fin clase
+
+/**
+ * //Equivalente.java
+    public class Equivalente {
+            public static void main( String [ ] args ) {
+                    Integer n1 = new Integer( 47 );
+                    Integer n2 = new Integer( 47 );
+                    System.out.println( n1==n2 );
+                    System.out.println( n1!=n2 );
+
+                    System.out.println(n2==n1);
+                    System.out.println(n2!=n1);
+                    }
+            }
+    //fin clase
+ */
