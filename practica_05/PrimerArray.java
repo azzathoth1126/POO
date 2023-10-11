f03945ffe8bfb5c44fd39ece8b8f589a2a3933da
Azzathoth1126 <alexis.mta2020@gmail.com>
2023-10-11 00:15:54 +0000

    lsito

---
+++ practica_05/PrimerArray.java
@@ -0,0 +1,15 @@
+class PrimerArray{
+   public static void main(String arg[]){
+      String nombres[] ={
+          "Juan",
+          "Maria",
+          "Antonio",
+          "Victoria"
+      };
+      nombres[2]="Alexis";
+      System.out.println(nombres[0]);
+      System.out.println(nombres[1]);
+      System.out.println(nombres[2]);
+      System.out.println(nombres[3]);
+   }
+}
