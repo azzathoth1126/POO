f7f6cd0d8cf5c11124be46428f5fec1fe5040f79
Azzathoth1126 <alexis.mta2020@gmail.com>
2023-10-13 17:07:54 +0000

    listo

---
+++ practica_06/AutomovilDeCarreras.java
@@ -0,0 +1,7 @@
+public class AutomovilDeCarreras{
+    Mecanico miMecanico = new Mecanico();
+    public void pintar(){
+	System.out.println("Estoy pintando el coche");
+	miMecanico.hazTalacha(this);
+    }
+}
