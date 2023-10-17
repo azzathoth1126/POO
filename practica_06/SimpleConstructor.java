b614fa44e70e5920f1404ce64d7e3bdb10dd53a8
Azzathoth1126 <alexis.mta2020@gmail.com>
2023-10-17 23:41:45 +0000

    listo

--- practica_06/SimpleConstructor.java
+++ practica_06/SimpleConstructor.java
@@ -3,7 +3,11 @@ class Rock {
 	System.out.println("Creando el constructor n˙mero: "+i);
 	}
 }
-
+/**
+ * Crea diez objeto pero no hay forma de acceder a ellos
+ * 
+ * No podemos acceder a ellos porque no sabemos sus nombres
+ */
 public class SimpleConstructor {
 	public static void main(String[ ] args) {
 	for(int i=0;i<10;i++)
