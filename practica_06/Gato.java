d14334012fa2eca03a1ad63f1822d684a9141a32
Azzathoth1126 <alexis.mta2020@gmail.com>
2023-10-18 00:00:30 +0000

    listo

--- practica_06/Gato.java
+++ practica_06/Gato.java
@@ -5,9 +5,5 @@ public class Gato extends Animales {
 }
 
 /**
- * public class Gato extends Animales {
-	public void maulla(){
-	System.out.println("Miauuu");
-	}
-}
+ * Herencia con Animales.java
  */
---
+++ practica_06/Perro.java
@@ -0,0 +1,10 @@
+public class Perro extends Animales {
+	public void ladra(){
+	System.out.println("Guauuuu");
+	}
+}
+
+
+/**
+ * Herencia con Animales.java
+ */
