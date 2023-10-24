e195dfe45e92d612fd84e14245af65d916cd9399
Azzathoth1126 <alexis.mta2020@gmail.com>
2023-10-24 23:27:55 +0000

    listo

--- practica_06/AtributosyMetodos.java
+++ practica_06/AtributosyMetodos.java
@@ -1,15 +1,19 @@
 class AtributosyMetodos{
 	//atributo
-	int a; //Global inicializada en null
+	int a; //Global inicializada en 0 o null
 	//metodos
+	
 	void noHagoNada(){
-		}
+	}
+
 	void imprimoHola(){
 		System.out.println("Hola");
-		}
+	}
+
 	public static void main(String args[]){
 		AtributosyMetodos am = new AtributosyMetodos();
 		am.noHagoNada();
 		am.imprimoHola();
-		}
+		System.out.println("a = "+ am.a); // Imprime el valor de a
+	}
 	}//fin clase
