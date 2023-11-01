a7d8136cc5068ec8de6955c06cfcd8511cc89a08
Azzathoth1126 <alexis.mta2020@gmail.com>
2023-11-01 00:22:28 +0000

    listo

--- practica_07/Unam.java
+++ practica_07/Unam.java
@@ -1,7 +1,7 @@
 public class Unam {
 
 	public static void main (String [] args) {
-		String cadena1="UNAM";
+		String cadena1="UNAM";  //grapers
 		String cadena2="UNAM";
 		String cadena3=new String("UNAM");
 		System.out.println (cadena1.equals(cadena2));
@@ -9,3 +9,10 @@ public class Unam {
 		System.out.println (cadena1.equals(cadena3));
 	}
 }
+
+
+// Concepto de boxing y umboxing
+// son tres objetos diferentes
+// equals sirve para comparar valores
+// == es para comparar objetos (localidad de memoria) 
+// pero como son metodos diferentes son false
