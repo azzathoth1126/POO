28953793133206a4d3e33ea97de2b4dc269603d5
Azzathoth1126 <alexis.mta2020@gmail.com>
2023-11-15 00:37:15 +0000

    listo

--- practica_03/Argumentos.java
+++ practica_03/Argumentos.java
@@ -1,7 +1,7 @@
 public class Argumentos {
     public static void main(String args[]){ 
 	if(args.length < 3){ 
-	    System.out.print("Hola, tu nombre es :"+args[0]);
+	    System.out.println("Hola, tu nombre es :"+args[0]);
 	    System.out.println(" y apellido: "+args[1]);
 	}
 	else 
